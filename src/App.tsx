88c61f4c0562ac8069d512915b4c9885a35575b3
Vaibhav <vaibhav@Vaibhavs-MacBook-Air.local>
2025-03-31 19:54:21 +0530

    auth0 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,44 @@ import { useAuth0 } from '@auth0/auth0-react'
 import './App.css'
 
 function App() {
-  const { loginWithRedirect } = useAuth0()
+  const { loginWithPopup, logout, user, isAuthenticated } = useAuth0()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => loginWithRedirect()}>
-          Login
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        !isAuthenticated ?
+          (
+            <>
+              <div>
+                <a href="https://vite.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              </div>
+              <h1>Vite + React</h1>
+              <div className="card">
+                <button onClick={() => loginWithPopup()}>
+                  Login
+                </button>
+                <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+              </div>
+              <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+              </p>
+            </>
+          ) : (
+            <>
+            <h1>Welcome {user?.name}</h1>
+            <button onClick={() => logout({ logoutParams: { returnTo: chrome.runtime.getURL("index.html") } })}>
+              Logout
+            </button>
+            </>
+          )
+      }
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import App from './App.tsx'
 
+const extensionUrl = chrome.runtime.getURL("index.html");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
       domain='dev-qi8m76xtwsxr8eif.us.auth0.com'
       clientId='if1c3MxroL2w7vlaOx1twjFA2lEGZEr8'
       authorizationParams={{
-        redirect_uri: "http://localhost:5173/"
+        redirect_uri: extensionUrl
       }}
+      cacheLocation='localstorage'
+      useRefreshTokens={true}
     >
       <App />
     </Auth0Provider>
